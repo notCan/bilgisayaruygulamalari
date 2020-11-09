f5ec211898b1e44b52ccae77a9f7cefd07c9bad2
Can <40603646+notCan@users.noreply.github.com>
2020-11-09 19:40:19 +0300

    v0.0.004 -card

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 //!colors
-//const mPrimaryColor = Color(0xff3f4edd);
-const mPrimaryColor = Color(0xff0c9869);
+const mPrimaryColor = Color(0xff3f4edd);
 const mTextColor = Color(0xff000000);
 const mBackgroundColor = Color(0xffffffff);
+const mButtonColor = Color(0xffC0C0C0);
+const mDefPadding = 20.0;
