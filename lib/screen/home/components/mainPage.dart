75c63eac7450b4295e1e1aa828c3e27858f6dbb9
Can <40603646+notCan@users.noreply.github.com>
2020-11-15 00:13:58 +0300

    v0.0.006 -complete mainpage template

--- lib/screen/home/components/mainPage.dart
+++ lib/screen/home/components/mainPage.dart
@@ -14,130 +14,272 @@ class MainPage extends StatelessWidget {
           //!Card Container
           WalletCard(),
           //!Fast Button
+          FastButtons(),
           Container(
-              height: size.height * 0.17,
-              width: size.width * 1,
-              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.4),
-                border: Border.all(color: mPrimaryColor, width: 5),
-                borderRadius: BorderRadius.all(Radius.circular(36)),
+            height: size.height * 0.384,
+            width: size.width * 1,
+            margin: EdgeInsets.symmetric(horizontal: 5.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(36)),
+                      color: Colors.green,
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 2.5),
+                    height: size.height * 0.12,
+                    child: Row(
+                      children: [
+                        //!Product Type
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Sağlık".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        //!Product Price
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "₺900".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(36)),
+                      color: Colors.pink,
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 2.5),
+                    height: size.height * 0.12,
+                    child: Row(
+                      children: [
+                        //!Product Type
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Eğlence".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        //!Product Price
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "₺2500".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(36)),
+                      color: Colors.blue,
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 2.5),
+                    height: size.height * 0.12,
+                    child: Row(
+                      children: [
+                        //!Product Type
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Temizlik".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        //!Product Price
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "₺400".toUpperCase(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.all(mDefPadding),
-                child: Row(
-                  children: [
-                    Container(
-                      //!ilk buton
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(100),
-                        ),
-                      ),
-                      margin: EdgeInsets.all(1.0),
-                      width: size.width * 0.22,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Column(
-                              children: <Widget>[
-                                Icon(Icons.add),
-                                Text("Gelir"),
-                              ],
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      //!ikinci buton
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(100),
-                        ),
-                      ),
-                      margin: EdgeInsets.all(1.0),
-                      width: size.width * 0.22,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset("assets/icons/minus.png"),
-                                Text("Gider"),
-                              ],
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      //!üçüncü buton
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(100),
-                        ),
-                      ),
-                      margin: EdgeInsets.all(1.0),
-                      width: size.width * 0.22,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset("assets/icons/percent.png"),
-                                Text("Faiz"),
-                              ],
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      //!dördüncü buton
-                      decoration: BoxDecoration(
-                        color: Colors.pink,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(100),
-                        ),
-                      ),
-                      margin: EdgeInsets.all(1.0),
-                      width: size.width * 0.22,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Column(
-                              children: <Widget>[
-                                Icon(Icons.credit_card),
-                                Text("Kredi"),
-                              ],
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ))
+            ),
+          ),
         ],
       ),
     );
+  }
+}
+
+class FastButtons extends StatelessWidget {
+  const FastButtons({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        height: size.height * 0.17,
+        width: size.width * 1,
+        margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.all(mDefPadding),
+          child: Row(
+            children: [
+              Container(
+                //!ilk buton
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+                margin: EdgeInsets.all(1.0),
+                width: size.width * 0.22,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.add),
+                          Text("Gelir"),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //!ikinci buton
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+                margin: EdgeInsets.all(1.0),
+                width: size.width * 0.22,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset("assets/icons/minus.png"),
+                          Text("Gider"),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //!üçüncü buton
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+                margin: EdgeInsets.all(1.0),
+                width: size.width * 0.22,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset("assets/icons/percent.png"),
+                          Text("Faiz"),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //!dördüncü buton
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+                margin: EdgeInsets.all(1.0),
+                width: size.width * 0.22,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.credit_card),
+                          Text("Taksit"),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
 
@@ -184,7 +326,7 @@ class WalletCard extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 17.0),
                   child: Align(
-                      alignment: Alignment.center,
+                      alignment: Alignment.centerLeft,
                       child: Text(
                         "Balance".toUpperCase(),
                         style: TextStyle(
